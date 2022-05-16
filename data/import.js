c98749537706a0b34c808769f2e594ec26ec7e3d
HMJ <hmj@goatpage.com>
2022-05-16 07:58:11 +0200

    import

--- data/import.js
+++ data/import.js
@@ -2,8 +2,11 @@ const { default: axios } = require('axios');
 const fs = require('fs');
 const Papa = require('papaparse');
 const moment = require("moment");
+const { Console } = require('console');
 
 const csvFilePath = 'system.csv'
+const csvSystem = 'system.csv'
+const csvServer = 'server.csv'
 
 // Function to read csv which returns a promise so you can do async / await.
 
@@ -48,8 +51,51 @@ const postSystem = (data) => {
        .catch(err => console.log(err,data));
 }
 
+const postServer = (data) => {
+  let server = { data :{
+    Nom: data["Libellé"].trim(),
+    Code : data["Code"].trim(),
+    Type : data["Type"].trim(),
+    VLAN : data["VLAN"].trim(),
+    IP : data["Adresse IP"].trim(),
+    Virtualisation : data["Virtualisation"].trim(),
+    OS : data["OS"].trim(),
+    Description : data["Description /role"].trim(),
+    Appli_code : data["1_Code Appli"].trim(),
+    Appli_libelle : data["2_Libellé Appli"].trim()
+    
+  }};
+  axios.get("http://localhost:1337/api/systems?filters[os_name][$eq]="+server.data.OS)
+       .then(resp=> {
+         if(resp.data.data.length==1){
+            console.log(resp.data.data[0].id)
+            server.data.OS=resp.data.data[0].id;
+            axios.get("http://localhost:1337/api/servers?filters[Code][$eq]="+server.data.Code)
+            .then( resp => {
+              if(resp.data.data.length==0){
+                axios.post("http://localhost:1337/api/servers",server)
+                .then(resp => console.log(server.data.Nom,"ADDED"))
+                .catch(err => console.log(err));
+               }else{
+                console.log(server.data.Nom,"EXISTS")
+               }
+            })
+            .catch(err => console.log(err,data));
 
-importCSV(csvFilePath,postSystem).then((result) => {
-    console.log(result);
+         }else{
+          console.log(server.data.OS,"NOT FOUND");
+         }
+       })
+       .catch(err => console.log(err,data));
+
+
+  
+}
+
+
+importCSV(csvSystem,postSystem).then((result) => {
+  console.log(result);
 });
-
+importCSV(csvServer,postServer).then((result) => {
+  
+});
